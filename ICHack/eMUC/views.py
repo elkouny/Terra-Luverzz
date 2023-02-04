22371b579070aa016e9cb666c2511a98f2be9a3e
shrutip2003 <pradhan2003shruti@gmail.com>
2023-02-04 22:53:48 +0000

    Form saves person information in the model

--- ICHack/eMUC/views.py
+++ ICHack/eMUC/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Person
 
 
 from .models import Picture
@@ -13,4 +14,13 @@ def pic_detail(request, pic_id):
     })
 
 def start(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        sex = request.POST.get('sex')
+
+        person = Person(name=name, age=age, sex=sex)
+        person.save()
+        #redirect('success')
+
     return render(request, 'start.html')

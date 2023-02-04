42b426c40e9c3a634bdffe666b178e9af363f3aa
Jacob <jjlarcombe007@outlook.com>
2023-02-04 20:42:27 +0000

    Finsihed photo attribute analysis code and test setup

--- deepface_age.py
+++ deepface_age.py
@@ -1,16 +1,31 @@
+import os
+import time
+
 from deepface import DeepFace
 from retinaface import RetinaFace
+from mtcnn import mtcnn
 import matplotlib.pyplot as plt
-
-faces = RetinaFace.extract_faces(img_path="TPDNE_2.jpg", align=True)
+import cv2
+import numpy as np
+imagePath = 'test_random_3.jpg'
+faces = DeepFace.extract_faces(img_path=imagePath, target_size=(300,300) ,align=True, detector_backend='mtcnn')
+print(len(faces))
 for face in faces:
-    objs = plt.imshow(face)
-    plt.savefig('TPDNE_2aligned.png')
-    plt.show()
-    plt.close()
+    print(face)
+facialCoords = faces[0]['facial_area']
+img = cv2.imread(imagePath)
+blank = np.zeros(img.shape[:2], dtype='uint8')
+startPoint = (round(facialCoords['x']*0.85), round(facialCoords['y']*0.85))
+endPoint = (round((facialCoords['x'] + facialCoords['w'])*1.15), round((facialCoords['y'] + facialCoords['h'])*1.15))
+mask = cv2.rectangle(blank, startPoint, endPoint, 255, -1)
+masked = cv2.bitwise_and(img, img, mask=mask)
+#cv2.imshow('Output', masked)
+cv2.imwrite('test.jpg', masked)
 
-objs = DeepFace.anaylze(img_path="",
+
+objs = DeepFace.analyze(masked,
     actions = ['age', 'gender', 'race','emotion'
-    ])
+   ])
 
 print(objs)
+

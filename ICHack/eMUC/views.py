d0edc5fe9982cd4567ee08b4cbc06fd6ad0a2849
hk221 <108066429+hk221@users.noreply.github.com>
2023-02-04 18:07:38 +0000

    Added skeleton of home page html

--- ICHack/eMUC/views.py
+++ ICHack/eMUC/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
-from .models import Person
+from .models import Picture
 
 def home(request):
     return render(request, 'home.html')
 
 def pic_detail(request, pic_id):
-    pic = Person.objects.get(id=pic_id)
+    pic = Picture.objects.get(id=pic_id)
     return render(request, 'pic_detail.html', {
         'pic': pic,
     })

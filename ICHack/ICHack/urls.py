d55740cb36e80b5b4a4c0c3e01b5b7ebb02f4758
hk221 <108066429+hk221@users.noreply.github.com>
2023-02-04 17:57:10 +0000

    urls and views

--- ICHack/ICHack/urls.py
+++ ICHack/ICHack/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from eMUC import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
+    path('eMUC/<int:pic_id>', views.pic_detail, name='pic_detail')
 ]
--- ICHack/eMUC/views.py
+++ ICHack/eMUC/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Person
+
+def home(request):
+    return render(request, 'home.html')
+
+def pic_detail(request, pic_id):
+    pic = Person.objects.get(id=pic_id)
+    return render(request, 'pic_detail.html', {
+        'pic': pic,
+    })

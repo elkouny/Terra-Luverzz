04c3b874749ba4cd67ab6f4e6788c0ff3e016878
shrutip2003 <pradhan2003shruti@gmail.com>
2023-02-04 16:32:11 +0000

    Change to picture migration with path

--- ICHack/eMUC/admin.py
+++ ICHack/eMUC/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Picture
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ['age', 'sex', 'ethnicity']
--- ICHack/eMUC/models.py
+++ ICHack/eMUC/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-class Person(models.Model):
+class Picture(models.Model):
     SEX_CHOICES = [('M','Male'), ('F', 'Female')]
+    photo_path = models.CharField(max_length= 200)
     age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     sex = models.CharField(max_length=1, choices= SEX_CHOICES)
     ethnicity = models.CharField(max_length=100)
-    height = models.DecimalField(null=True, decimal_places=2, max_digits= 5)
-    weight = models.DecimalField(null=True, decimal_places=2, max_digits= 5)
 
 
